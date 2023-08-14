8ddf5f2dabd93cfa1d75affa8f30246ca9868da4
agustinramirez1 <agusramirez2001@gmail.com>
2023-08-14 17:44:42 -0400

    Initial Commit

---
+++ TicTacToe/ViewController.swift
@@ -0,0 +1,164 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by Agustin Ramirez on 2023-08-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    enum Turn {
+        case Circulo
+        case Cruz
+    }
+    
+    @IBOutlet weak var turnLabel: UILabel!
+    @IBOutlet weak var a1: UIButton!
+    @IBOutlet weak var a2: UIButton!
+    @IBOutlet weak var a3: UIButton!
+    @IBOutlet weak var b1: UIButton!
+    @IBOutlet weak var b2: UIButton!
+    @IBOutlet weak var b3: UIButton!
+    @IBOutlet weak var c1: UIButton!
+    @IBOutlet weak var c2: UIButton!
+    @IBOutlet weak var c3: UIButton!
+    
+    var firstTurn = Turn.Cruz
+    var currentTurn = Turn.Cruz
+    
+    var CIRCULO = "O"
+    var CRUZ = "X"
+    var board = [UIButton]()
+    var circuloScore = 0
+    var cruzScore = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initBoard()
+        resetBoard()
+    }
+    
+    func initBoard() {
+        board.append(a1)
+        board.append(a2)
+        board.append(a3)
+        board.append(b1)
+        board.append(b2)
+        board.append(b3)
+        board.append(c1)
+        board.append(c2)
+        board.append(c3)
+    }
+
+    @IBAction func boardTapAction(_ sender: UIButton) {
+        addToBoard(sender)
+        
+        if checkForVictory(CRUZ) {
+            cruzScore += 1
+            resultAlert(title: "Gana X!")
+        }
+        if checkForVictory(CIRCULO) {
+            circuloScore += 1
+            resultAlert(title: "Gana O!")
+        }
+        
+        if(fullBoard()) {
+            resultAlert(title: "Draw")
+        }
+    }
+    
+    func checkForVictory(_ s: String) -> Bool {
+        
+        //Horizontal Victory
+        if thisSymbol(a1, s) && thisSymbol(a2, s) && thisSymbol(a3, s) {
+            return true
+        }
+        if thisSymbol(b1, s) && thisSymbol(b2, s) && thisSymbol(b3, s) {
+            return true
+        }
+        if thisSymbol(c1, s) && thisSymbol(c2, s) && thisSymbol(c3, s) {
+            return true
+        }
+        
+        //Vertical Victory
+        if thisSymbol(a1, s) && thisSymbol(b1, s) && thisSymbol(c1, s) {
+            return true
+        }
+        if thisSymbol(a2, s) && thisSymbol(b2, s) && thisSymbol(c2, s) {
+            return true
+        }
+        if thisSymbol(a3, s) && thisSymbol(b3, s) && thisSymbol(c3, s) {
+            return true
+        }
+        
+        //Diagonal Victory
+        if thisSymbol(a1, s) && thisSymbol(b2, s) && thisSymbol(c3, s) {
+            return true
+        }
+        if thisSymbol(a3, s) && thisSymbol(b2, s) && thisSymbol(c1, s) {
+            return true
+        }
+        
+        return false
+    }
+    
+    func thisSymbol(_ button: UIButton, _ symbol: String) -> Bool {
+        
+        
+        return button.title(for: .normal) == symbol
+    }
+    
+    func resultAlert(title: String) {
+        let message = "\nCirculo " + String(circuloScore) + "\n\nCruz " + String(cruzScore)
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Reset", style: .default, handler: { (_) in
+            self.resetBoard()
+        }))
+        self.present(ac, animated: true)
+    }
+    
+    func resetBoard() {
+        for button in board {
+            button.setTitle(nil, for: .normal)
+            button.isEnabled = true
+        }
+        if (firstTurn == Turn.Circulo) {
+            firstTurn = Turn.Cruz
+            turnLabel.text = CRUZ
+        }
+        else if (firstTurn == Turn.Cruz) {
+            firstTurn = Turn.Circulo
+            turnLabel.text = CIRCULO
+        }
+        currentTurn = firstTurn
+    }
+    
+    func fullBoard() -> Bool {
+        for button in board {
+            if button.title(for: .normal) == nil{
+                return false
+            }
+        }
+        return true
+    }
+    
+    func addToBoard(_ sender: UIButton) {
+        if(sender.title(for: .normal) == nil) {
+            if(currentTurn == Turn.Circulo) {
+                sender.setTitle(CIRCULO, for: .normal)
+                currentTurn = Turn.Cruz
+                turnLabel.text = CRUZ
+            } else if (currentTurn == Turn.Cruz) {
+                sender.setTitle(CRUZ, for: .normal)
+                currentTurn = Turn.Circulo
+                turnLabel.text = CIRCULO
+            }
+            
+            sender.isEnabled = false
+        }
+    }
+    
+}
+
